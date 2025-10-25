18823264ed94331f091b8b8f0eeaca3fd2042989
Ankit_kumar <ankitchahar88@gmail.com>
2025-10-25 02:02:28 -0400

    Update delete_user.php

--- LMSPHP_week7/delete_user.php
+++ LMSPHP_week7/delete_user.php
@@ -6,13 +6,11 @@ secure();
 if(isset($_GET['id'])){
     $id = $_GET['id'];
     
-    // Get the image to delete from uploads folder
     $user = mysqli_fetch_assoc(mysqli_query($connect, "SELECT * FROM users WHERE id=$id"));
     if($user['image'] && file_exists('uploads/'.$user['image'])){
         unlink('uploads/'.$user['image']);
     }
 
-    // Delete user from database
     mysqli_query($connect, "DELETE FROM users WHERE id=$id");
 
     set_message('User deleted successfully!', 'success');
@@ -21,3 +19,4 @@ if(isset($_GET['id'])){
 header('Location: users.php');
 exit;
 ?>
+
